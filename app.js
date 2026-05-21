0bda03155cd5be7b7162f8f2b78b652c2aa7e805
DY <alsrb1582@gmail.com>
2026-05-21 20:01:21 +0900

    chrome 개발자 도구 켰을 때 뜨는 오류 무시

--- app.js
+++ app.js
@@ -4,12 +4,20 @@ const path = require("path");
 const connectDB = require("./config/database");
 const session = require("express-session");
 const passport = require("./config/passport");
-const { errorHandler, notFoundHandler } = require("./middlewares/errorMiddleware");
+const {
+  errorHandler,
+  notFoundHandler,
+} = require("./middlewares/errorMiddleware");
 
 const app = express();
 
 // DB 연결
 connectDB();
+
+// chrome 개발자 도구 켰을 때 뜨는 오류 무시
+app.get("/.well-known/appspecific/com.chrome.devtools.json", (req, res) => {
+  res.status(204).end(); // 콘텐트 없음으로 정상 응답 처리
+});
 
 // 뷰 엔진
 app.set("view engine", "ejs");
