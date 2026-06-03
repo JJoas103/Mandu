37f3afbe8fbacbd519566cf7d9e300446db26de1
sukyum <kyumi1003@naver.com>
2026-06-03 17:23:43 +0900

    모임,제보게시판통합

--- models/Feed.js
+++ models/Feed.js
@@ -2,6 +2,14 @@ const mongoose = require('mongoose');
 
 const feedSchema = new mongoose.Schema(
     {
+        content: { type: String, required: true },
+        author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+        locationTag: { type: String, required: true }, // 장소 태그 필수
+        image: { type: String }, // 제보 사진
+        reactions: {
+            like: { type: Number, default: 0 },
+            thanks: { type: Number, default: 0 }
+        }, 
         place_id: {
             type: String,
             required: true
